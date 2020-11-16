bbfc2c85dfd6b690484d5eafe5df455caf6b4549
edwinmejia-123 <edwin_adonay123@hotmail.com>
2020-11-16 01:40:38 -0600

    Creacion de la modal para muestra de detalles

--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,21 @@
-var titulo, precio;
+var titulo, precio, id;
+
 $(".enlace").on("click", function () {
-  titulo = $(this).val();
-  console.log(titulo);
-  document.getElementById("exampleModalCenterTitle").innerHTML =
-    "Titulo " + $(this).val();
-});
-$(".enlace").on("click", function () {
-  titulo = $(this).val();
-  console.log(precio);
-  document.getElementById("exampleModalCenterTitle").innerHTML =
-    "$ " + $(this).val();
-});
-$(".imagen").click(function (e) {
-  var img = e.target.src;
+  id = $(this).val();
+  console.log(id);
   var modal_imagen = "";
-  var cont = "";
+  var img;
+  titulo = document.getElementById(id).getElementsByClassName("titulo").item(0)
+    .textContent;
+  precio = document.getElementById(id).getElementsByClassName("precio").item(0)
+    .textContent;
+  img = document
+    .getElementById(id)
+    .getElementsByClassName("imagen")
+    .item(0)
+    .getAttribute("src");
   modal_imagen += '<img src="' + img + '" alt="" class="imagen_modal"></img>';
   document.getElementById("modal-img").innerHTML = modal_imagen;
-  //document.getElementById("exampleModalCenterTitle").innerHTML = cont;
+  document.getElementById("titulo").innerHTML = titulo;
+  document.getElementById("precio").innerHTML = precio;
 });
