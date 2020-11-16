ff79378e1d9e0159c3b724d43f5cc24d900c98ae
edwinmejia-123 <edwin_adonay123@hotmail.com>
2020-11-15 23:06:11 -0600

    Modal

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+var titulo, precio;
+$(".enlace").on("click", function () {
+  titulo = $(this).val();
+  console.log(titulo);
+  document.getElementById("exampleModalCenterTitle").innerHTML =
+    "Titulo " + $(this).val();
+});
+$(".enlace").on("click", function () {
+  titulo = $(this).val();
+  console.log(precio);
+  document.getElementById("exampleModalCenterTitle").innerHTML =
+    "$ " + $(this).val();
+});
+$(".imagen").click(function (e) {
+  var img = e.target.src;
+  var modal_imagen = "";
+  var cont = "";
+  modal_imagen += '<img src="' + img + '" alt="" class="imagen_modal"></img>';
+  document.getElementById("modal-img").innerHTML = modal_imagen;
+  //document.getElementById("exampleModalCenterTitle").innerHTML = cont;
+});
